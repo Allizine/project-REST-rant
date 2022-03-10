0b714af202c6db3bea2cc27ee06f95a5c0219bfb
Soullessly <alexbarthel01@gmail.com>
2022-03-09 21:04:57 -0600

    minor changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'jsx')
-app.engine('jax' require('express-react-views').createEngine())
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'))
 
