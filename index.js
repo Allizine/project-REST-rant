e11120c922932b8c30b20166840e8dbcc3557c17
Soullessly <alexbarthel01@gmail.com>
2022-03-14 19:45:42 -0500

    Cannot view added page

--- index.js
+++ index.js
@@ -12,21 +12,16 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-
 // Controllers & Routes
-//Places Page
 app.use('/places', require('./controllers/places'))
 
-//Home Page
 app.get('/', (req, res) => {
     res.render('home')
 })
-//404 Page
+
 app.get('*', (req, res) => {
     res.render('error404')
 })
 
 // Listen for Connections
 app.listen(process.env.PORT)
-
-
