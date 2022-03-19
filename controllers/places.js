1dc97dd45eab52fcd605f77f49719f767c05a3de
Soullessly <alexbarthel01@gmail.com>
2022-03-19 13:39:50 -0500

    MongoDB Fixed

--- controllers/places.js
+++ controllers/places.js
@@ -1,18 +1,16 @@
-const router = require("express").Router();
+const router = require("express").Router()
 const db = require('../models')
 
-const router = require('express').Router()
-
 router.get('/', (req, res) => {
-  
   db.Place.create(req.body)
   .then (() => {
-    res.redirect('/places/index', { places })
+    res.redirect('/places/index'), { places } 
   })
   .catch (err => {
     console.error('err', err)
     res.render('error404')
   })
+},
 
 router.post('/', (req, res) => {
   db.Place.create(req.body)
@@ -23,40 +21,41 @@ router.post('/', (req, res) => {
     console.error('err', err)
     res.render('error404')
 })
+},
 
 router.get('/new', (req, res) => {
   res.render('places/new')
-})
+}),
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
   .then(place => {
-    res.render('places/show' { places })
+    res.render('places/show'), { place }
   })
   .catch (err => {
     console.log('err', err)
     res.render('error404')
   })
-})
+}),
 
 router.put('/:id', (req, res) => {
   res.send('PUT /places/:id stub')
-})
+}),
 
 router.delete('/:id', (req, res) => {
   res.send('DELETE /places/:id stub')
-})
+}),
 
 router.get('/:id/edit', (req, res) => {
   res.send('GET edit form stub')
-})
+})),
 
 router.post('/:id/rant', (req, res) => {
   res.send('GET /places/:id/rant stub')
-})
+}),
 
 router.delete('/:id/rant/:rantId', (req, res) => {
     res.send('GET /places/:id/rant/:rantId stub')
-})
+})),
 
-module.exports = router;
+module.exports = router
