15902f50d63b7124b4dd5ee29f7c860e90674d35
Soullessly <alexbarthel01@gmail.com>
2022-03-26 11:57:41 -0500

    Rest rant 10

--- controllers/places.js
+++ controllers/places.js
@@ -7,7 +7,6 @@ router.get("/", (req, res) => {
       res.render("places/index", { places });
     })
     .catch((err) => {
-      console.log(err);
       res.render("error404");
     });
 });
@@ -38,7 +37,9 @@ router.get("/new", (req, res) => {
 
 router.get("/:id", (req, res) => {
   db.Place.findById(req.params.id)
+    .populate("comments")
     .then((place) => {
+      console.log(place.comments);
       res.render("places/show", { place });
     })
     .catch((err) => {
@@ -57,6 +58,32 @@ router.delete("/:id", (req, res) => {
 
 router.get("/:id/edit", (req, res) => {
   res.send("GET edit form stub");
+});
+
+router.post("/:id/comment", (req, res) => {
+  if (req.body.rant) {
+    req.body.rant = true;
+  } else {
+    req.body.rant = false;
+  }
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      console.log(db.Comment);
+      db.Comment.create(req.body)
+        .then((comment) => {
+          place.comments.push(comment.id);
+          place.save().then(() => {
+            res.redirect(`/places/${req.params.id}`);
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.render("error404");
+        });
+    })
+    .catch((err) => {
+      res.render("error404");
+    });
 });
 
 router.post("/:id/rant", (req, res) => {
